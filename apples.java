70b905dacb6474631501444e920161ee8a769431
ssroydas <cloud9b@gmail.com>
2015-05-19 20:29:44 -0400

    Create apples.java
    program for test

---
+++ apples.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+import java.util.Random;
+import bucky.tuna;
+
+
+public class apples 
+{
+	
+			
+		public static void main(String[] args)
+		{
+			int bucky[] = {3,4,5,6,7};
+			int total = 0;
+			
+			for (int x: bucky)
+			{
+				total +=x;
+			}
+			System.out.println(total);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+			
+}
+	
+/*{
+		
+		Scanner input =	new Scanner (System.in);
+		tuna tunaObject = new tuna();
+		
+		System.out.println("Enter your name here");
+		String name = input.nextLine();
+		
+		tunaObject.simpleMessage(name);
+		}
+		*/
